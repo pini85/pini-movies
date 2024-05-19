cd2b1e397203c4760b652fe9279188ee4913748a
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-05-19 14:59:57 +0300

    testing server pipeline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useQuery } from 'react-query';
-import { setUser } from './redux/slices/user.slice.ts';
-import { useDispatch } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useQuery } from "react-query";
+import { setUser } from "./redux/slices/user.slice.ts";
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import ScrollIntoView from './components/ScrollIntoView.component';
-import RouteConfig from './routes/RouteConfig';
-import { currentUser } from 'apis/constants';
+import ScrollIntoView from "./components/ScrollIntoView.component";
+import RouteConfig from "./routes/RouteConfig";
+import { currentUser } from "apis/constants";
 
 const App = () => {
-  const [authToken, setToken] = useState(localStorage.getItem('authToken'));
+  const [authToken, setToken] = useState(localStorage.getItem("authToken"));
 
   const theme = useSelector((state) => state.ui.theme);
-  const userId = useSelector((state) => state.user?.user?._id);
+
   const dispatch = useDispatch();
-  const { data: user } = useQuery('currentUser', currentUser, {
+  const { data: user } = useQuery("currentUser", currentUser, {
     enabled: !!authToken,
   });
 
@@ -30,7 +30,7 @@ const App = () => {
   useEffect(() => {
     window.document
       .querySelector('meta[name="theme-color"]')
-      .setAttribute('content', '#201021');
+      .setAttribute("content", "#201021");
   }, [theme]);
   useEffect(() => {}, []);
 

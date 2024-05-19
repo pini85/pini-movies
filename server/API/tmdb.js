78df5e785df0e87b9123c1772fc65b3c4264db7a
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-05-19 14:55:10 +0300

    Update tmdb.js

--- server/API/tmdb.js
+++ server/API/tmdb.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const getTmdbWithId = async (id) => {
   const { data } = await axios.get(
@@ -8,6 +8,7 @@ const getTmdbWithId = async (id) => {
   return data;
 };
 const getMovieByTitle = async (title) => {
+  console.log("title", title);
   const { data } = await axios.get(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${title}`
   );

7bec5b6e953fb1c278166248c3b884e965f5f3cb
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2024-05-26 20:32:33 +0300

    fixed checkbox values for occasion

--- client/src/pages/AIRecommendation/components/FindRecommendations/utils.js
+++ client/src/pages/AIRecommendation/components/FindRecommendations/utils.js
@@ -56,11 +56,7 @@ export const recommendationQuestions = [
         content: "Movie Night with friends",
         value: "friends",
       },
-      {
-        type: "checkbox",
-        content: "Date Night with boyfriend or girlfriend",
-        value: "date",
-      },
+      { type: "checkbox", content: "Family Movie Night", value: "family" },
       { type: "checkbox", content: "Kids' Movie Night", value: "kids" },
       {
         type: "selector",
